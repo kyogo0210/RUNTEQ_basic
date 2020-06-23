a2aca369f619806b1d05d646bf8ff823051ec334
山田敬吾 <yamadakyogo@yamadakeigonoMacBook-Air.local>
2020-06-23 20:45:43 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def not_authenticated
-    redirect_to login_url, danger: 'ログインしてください'
+    flash[:danger] = 'ログインしてください'
+    redirect_to login_url
   end
 end
